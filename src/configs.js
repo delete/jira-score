8bfb87c2d8bbe62dd7b5a9ed36c497ebfdb78f75
delete <pinheiro.llip@gmail.com>
2017-07-05 19:33:41 -0300

    get user from config file

--- src/configs.js
+++ src/configs.js
@@ -11,7 +11,7 @@ const auth = () => to64(config.login, config.pass)
 
 const _url = () => `http://${config.domain}/issues/`
 
-const url = ( startDate='2017-06-01', endDate='2017-06-30', user='fellipe.dsn.cir' ) => _url() + encodeURI(`?jql=category = Cirrus AND "Dificuldade de Implementação" is not EMPTY AND resolution = Resolvido AND assignee = ${user} AND resolved >= ${startDate} AND resolved <= "${endDate} 23:59" ORDER BY cf[17132] ASC, resolved DESC`)
+const url = ( startDate='2017-06-01', endDate='2017-06-30', user=config.login ) => _url() + encodeURI(`?jql=category = Cirrus AND "Dificuldade de Implementação" is not EMPTY AND resolution = Resolvido AND assignee = ${user} AND resolved >= ${startDate} AND resolved <= "${endDate} 23:59" ORDER BY cf[17132] ASC, resolved DESC`)
 
 module.exports = {
     auth,
