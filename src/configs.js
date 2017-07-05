db9a5a9b218fb97d20438b22dc66e1c5cef79326
delete <pinheiro.llip@gmail.com>
2017-07-05 09:31:27 -0300

    add dynamic filter

--- src/configs.js
+++ src/configs.js
@@ -9,10 +9,11 @@ const to64 = ( login, pass ) => toBase64(`${login}:${pass}`)
 
 const auth = () => to64(config.login, config.pass)
 
-const filterUrl = () => `http://${config.domain}/issues/?filter=${config.filter_id}`
-const query = ( startDate, endDate, user ) => `&jql=category%20%3D%20Cirrus%20AND%20%22Dificuldade%20de%20Implementa%C3%A7%C3%A3o%22%20is%20not%20EMPTY%20AND%20resolution%20%3D%20Resolvido%20AND%20assignee%20%3D%20${user}%20AND%20resolved%20%3E%3D%20${startDate}%20AND%20resolved%20%3C%3D%20%22${endDate}%2023%3A59%22%20ORDER%20BY%20cf%5B17132%5D%20ASC%2C%20resolved%20DESC`
+const _url = () => `http://${config.domain}/issues/`
 
 const url = (startDate, endDate, user=config.login) => filterUrl() + query( startDate, endDate, user )
+
+const url = ( startDate='2017-06-01', endDate='2017-06-30', user='fellipe.dsn.cir' ) => _url() + encodeURI(`?jql=category = Cirrus AND "Dificuldade de Implementação" is not EMPTY AND resolution = Resolvido AND assignee = ${user} AND resolved >= ${startDate} AND resolved <= "${endDate} 23:59" ORDER BY cf[17132] ASC, resolved DESC`)
 
 module.exports = {
     auth,
