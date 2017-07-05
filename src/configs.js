4a4dd8da781481813ab6d18c9c1fd9787dd972eb
delete <pinheiro.llip@gmail.com>
2017-07-05 19:05:49 -0300

    fix conflito

--- src/configs.js
+++ src/configs.js
@@ -11,8 +11,6 @@ const auth = () => to64(config.login, config.pass)
 
 const _url = () => `http://${config.domain}/issues/`
 
-const url = (startDate, endDate, user=config.login) => filterUrl() + query( startDate, endDate, user )
-
 const url = ( startDate='2017-06-01', endDate='2017-06-30', user='fellipe.dsn.cir' ) => _url() + encodeURI(`?jql=category = Cirrus AND "Dificuldade de Implementação" is not EMPTY AND resolution = Resolvido AND assignee = ${user} AND resolved >= ${startDate} AND resolved <= "${endDate} 23:59" ORDER BY cf[17132] ASC, resolved DESC`)
 
 module.exports = {
