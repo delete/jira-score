9f5bd454605a5ac0edb1af647aaf82ecec3b1268
delete <pinheiro.llip@gmail.com>
2017-07-06 11:16:43 -0300

    update index to use the new API

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ const printIssue = issue => console.log( `${issue.key} -> ${issue.difficulty} ->
 const print = data => {
     const jira = parser(data)
     const issues = jira.scoredIssues()
+    const issuesByDifficulty = jira.totalIssuesByDifficulty
 
     issues.map( printIssue )
 
-    console.log(`\n\nTotal Not Classified Issues: ${jira.notClassifiedIssues()}`)
-    console.log(`Total Very Simple Issues: ${jira.verySimpleIssues()}`)
-    console.log(`Total Simple Issues: ${jira.simpleIssues()}`)
-    console.log(`Total Medium Issues: ${jira.mediumIssues()}`)
-    console.log(`Total Hard Issues: ${jira.hardIssues()}`)
-    console.log(`Total Very Hard Issues: ${jira.veryHardIssues()}`)
+    console.log(`\n\nTotal Not Classified Issues: ${issuesByDifficulty.notClassified}`)
+    console.log(`Total Very Simple Issues: ${issuesByDifficulty.verySimple}`)
+    console.log(`Total Simple Issues: ${issuesByDifficulty.simple}`)
+    console.log(`Total Medium Issues: ${issuesByDifficulty.medium}`)
+    console.log(`Total Hard Issues: ${issuesByDifficulty.hard}`)
+    console.log(`Total Very Hard Issues: ${issuesByDifficulty.veryHard}`)
     console.log(`Total Customer Service: ${jira.customerService()}`)
     console.log(`Total Customer Service Time: ${jira.customerServiceTime()} minutes`)
     console.log(`Total issues: ${jira.scored()}`)
@@ -26,8 +27,8 @@ const print = data => {
     console.log(`Goal: ${goal()}`)
 }
 
-const startDate = '2017-06-01'
-const endDate = '2017-06-30'
+const startDate = '2017-07-01'
+const endDate = '2017-07-31'
 
 const filterUrl = url( startDate, endDate )
 const headers = { 'Authorization': `Basic ${auth()}` }
