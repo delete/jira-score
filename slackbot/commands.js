9d887573adbfd3bfbb5ff2bed66cacdeda4e3f39
delete <pinheiro.llip@gmail.com>
2017-07-13 21:03:53 -0300

    close issue #24

--- slackbot/commands.js
+++ slackbot/commands.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const get = require('../src/request')
-const { auth, url, goal } = require('../src/configs')
+const { auth, url, goal, pointsMinute } = require('../src/configs')
 const parser = require('../src/parser')
 const { 
     countIssuesByType,
@@ -28,14 +28,23 @@ const score = ( user ) => {
     return loadIssues( user )
         .then( issues => {
             const objective = goal(user)
-            const pontuation = sumPontuation( issues )
+            let pontuation = sumPontuation( issues )
+
+            const cst = sumTime( issues, 'Atendimento' )
+            const taskstime = sumTime( issues, 'Tarefa' )
+            const totalTime = cst + taskstime
+            const timeInPoints = Math.round( totalTime * pointsMinute( user ) )
+
+            pontuation = pontuation + timeInPoints
+
             const percentage = ( (pontuation * 100) / objective ).toFixed(2);
             const moreThanHalf = 'Tu ta o bichão memo, em?!'
             const lessThanHalf = 'Anda logo com isso ae!'
             const lessThanOneThird = 'Trabalha não?! É bom começar.'
             
             const response = [
-                `Você tem *${pontuation}* pontos e completou *${percentage}%* da meta *${objective}* !`,
+                `Você fez *${totalTime} minutos*, o que da *${timeInPoints}* pontos`,
+                `Você tem no total *${pontuation}* pontos e completou *${percentage}%* da meta *${objective}* !`,
                 `Faltam *${objective - pontuation}* pontos, *${(100 - percentage).toFixed(2)}%* para bater a meta!`,
                 `\n${percentage < 33 ? lessThanOneThird : percentage < 50 ? lessThanHalf : moreThanHalf}`
             ]
@@ -55,11 +64,15 @@ const issues = ( user ) => {
             const h = countIssuesByDifficulty( issues, 'Difícil')
             const vh = countIssuesByDifficulty( issues, 'Muito difícil')
 
+            const pointsPerMinute = pointsMinute( user )
+
             const cs = countIssuesByType( issues, 'Atendimento')
             const cst = sumTime( issues, 'Atendimento' )
+            const cstp = Math.round( cst * pointsPerMinute )
 
             const tasks = countIssuesByType( issues, 'Tarefa')
             const taskstime = sumTime( issues, 'Tarefa' )
+            const taskstimepoint = Math.round( taskstime * pointsPerMinute )
 
             const scored = scoredIssues( issues )
 
@@ -71,9 +84,9 @@ const issues = ( user ) => {
                 `Issues Difíceis: *${h}*`,
                 `Issues Muito díficeis: *${vh}*`,
                 `\nIssues de Atendimento: *${cs}*`,
-                `Total de tempo em issues de Atendimento: *${cst} minutos*`,
+                `Total de tempo em issues de Atendimento: *${cst} minutos* - ${cstp} pontos`,
                 `\nIssues de Tarefas: *${tasks}*`,
-                `Total de tempo em issues de Tarefas: *${taskstime} minutos*`,
+                `Total de tempo em issues de Tarefas: *${taskstime} minutos* - ${taskstimepoint} pontos`,
                 `\nTotal de issues: *${issues.length}*`,
                 `Total de issues pontuadas: *${scored.length}*`
             ]
