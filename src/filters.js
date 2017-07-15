366c5b67222d1933edb3341319714b164f1f0d10
delete <pinheiro.llip@gmail.com>
2017-07-15 17:29:21 -0300

    format some code

--- src/filters.js
+++ src/filters.js
@@ -2,10 +2,18 @@
 
 const { isClassified } = require('./pontuations')
 
-const countIssuesByType = ( issues, type ) => issues.reduce( (total, issue ) => issue.type === type ? total + 1 : total, 0 )
-const countIssuesByDifficulty = ( issues, difficulty ) => issues.reduce( (total, issue ) => issue.difficulty === difficulty && isClassified(issue.type) ? total + 1 : total, 0 )
-const sumPontuation = ( issues ) => issues.reduce( (total, issue ) => isClassified(issue.type) ? total + issue.pontuation : total, 0 )
-const sumTime = ( issues, type ) => issues.reduce( (total, issue ) => issue.type === type ? total + issue.time : total, 0 )
+const countIssuesByType = ( issues, type ) => 
+    issues.reduce( (total, issue ) => issue.type === type ? total + 1 : total, 0 )
+
+const countIssuesByDifficulty = ( issues, difficulty ) => 
+    issues.reduce( (total, issue ) => issue.difficulty === difficulty && isClassified(issue.type) ? total + 1 : total, 0 )
+
+const sumPontuation = ( issues ) => 
+    issues.reduce( (total, issue ) => isClassified(issue.type) ? total + issue.pontuation : total, 0 )
+
+const sumTime = ( issues, type ) => 
+    issues.reduce( (total, issue ) => issue.type === type ? total + issue.time : total, 0 )
+
 const hasScore = ( issue ) => issue.pontuation > 0 && isClassified(issue.type)
 const scoredIssues = ( issues ) => issues.filter( issue => hasScore( issue ) )
 
