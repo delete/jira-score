6cf0a0bc0b1372aa852a03eefeda7fe47f4fbc0b
delete <pinheiro.llip@gmail.com>
2017-07-18 20:35:44 -0300

    separate query into functions

---
+++ tests/query.test.js
@@ -0,0 +1,15 @@
+const makeQuery = require('../src/query')
+
+describe('Function: fast query', () => {
+    test('Must return the hole query with the right inputs"', () => {
+        const user = 'iamanuser'
+        const startDate = '2017-06-01'
+        const endDate = '2017-06-30'
+
+        const result = makeQuery( user, startDate, endDate )
+        
+        const expected = '?jql=category = Cirrus AND "Dificuldade de Implementação" is not EMPTY AND resolution = Resolvido AND assignee = iamanuser AND resolved >= 2017-06-01 AND resolved <= "2017-06-30 23:59" ORDER BY cf[17132] ASC, resolved DESC'
+
+        expect(result).toBe(expected)
+    })
+})
