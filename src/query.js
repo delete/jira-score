3084777ccc5e233790af7b55042f4a5add4aed0a
delete <pinheiro.llip@gmail.com>
2017-07-31 17:39:04 -0300

    get analisys issues

--- src/query.js
+++ src/query.js
@@ -8,8 +8,9 @@ const moreOrEqualThan = () => '>='
 const orderBy = ( string ) => `ORDER BY ${string}`
 const assignee = ( user ) => `assignee = ${user}`
 
+// module.exports = ( user, startDate, endDate ) => `?jql=category = Cirrus AND issuetype not in (Epic) AND status changed to (Pronto, Finalizado, "Finalizado / Liberado") during (${startDate}, "${endDate} 23:59")  AND status was not in (Pronto, Finalizado, "Finalizado / Liberado") before ${startDate} AND assignee in (${user})`
 module.exports = ( user, startDate, endDate ) =>
-    [
+ [
         `?jql=category = Cirrus`,
         `${and()}`,
         `"Dificuldade de Implementação" ${isNot()} ${empty()}`,
