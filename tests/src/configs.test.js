28a7e5c7bafbed2a1287d3637df39b9872e04dba
delete <pinheiro.llip@gmail.com>
2017-08-01 20:25:07 -0300

    remove prodution test

--- tests/src/configs.test.js
+++ tests/src/configs.test.js
@@ -1,13 +1,15 @@
 process.env.ENV_DEV = 'false'
-const configs = require('../../src/configs')
-const { loadFile } = require('../../src/utils')
 
-const ENV_FILE = './tests/fixtures/env_test'
+const { loadFile } = require('../../src/utils')
+const ENV_FILE_DEV = './tests/fixtures/env_test'
+
+const configs = require('../../src/configs')
+
 
 describe('Test configs file', () => {
     test('File must have the expected data', done => {
 
-        loadFile(ENV_FILE, (err, data) => {
+        loadFile(ENV_FILE_DEV, (err, data) => {
             if (err) throw err
             
             const dataObj = JSON.parse(data)
@@ -32,16 +34,11 @@ describe('Test configs file', () => {
 })
 
 describe('Test with ENV_DEV variable', () => {
+
     test('isDev method must return false', () => {
         const actual = configs.isDev
         const expected = false
         expect( actual ).toBe( expected)
     })
 
-    test('Url must return the entire path to prod environment', () => {
-        const actual = configs.url()
-        const expected = "http://127.0.0.1:8080/issues.json/rest/api/2/search?jql=category%20=%20Cirrus%20AND%20%22Dificuldade%20de%20Implementa%C3%A7%C3%A3o%22%20is%20not%20EMPTY%20AND%20resolution%20=%20Resolvido%20AND%20assignee%20=%20fellipe.dsn.cir%20AND%20resolved%20%3E=%202017-06-01%20AND%20resolved%20%3C=%20%222017-06-30%2023:59%22%20ORDER%20BY%20cf%5B17132%5D%20ASC,%20resolved%20DESC&fields=assignee,project,customfield_21711,issuetype,timespent,customfield_17132&maxResults=200"
-        expect( actual ).toBe( expected)
-    })
 })
-
