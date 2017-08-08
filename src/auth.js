a5af56552cfbfafe7e28556213f30fd3bf9d2fc8
delete <pinheiro.llip@gmail.com>
2017-08-07 22:00:36 -0300

    create a modle to create the url

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const { toBase64 } = require('./utils')
 const to64 = ( login, pass ) => toBase64( `${login}:${pass}` )
 const auth = (login, pass) => to64( login, pass )
--- src/configs.js
+++ src/configs.js
@@ -1,21 +1,16 @@
 'use strict'
 
 const { loadFileSync } = require('./utils')
-const makeQuery = require('./query')
+const urlProd = require('./url')
 
 const isDev = process.env.ENV_DEV == 'true'
 const filename = isDev ? './tests/fixtures/env_test' : './.env'
 const config = JSON.parse( loadFileSync( filename, "utf8") )
 const isDSN = ( user ) => /.*dsn.*/.test(user)
 
-const _url = () => `http://${config.domain}/rest/api/2/search`
-const getParams = () => '&fields=assignee,project,customfield_21711,issuetype,timespent,customfield_17132&maxResults=200'
-const urlProd = ( startDate, endDate, user ) => 
-    [`${_url()}`, encodeURI( `${makeQuery( user, startDate, endDate ).faster()}${getParams()}` ) ].join('')
-
 const urlDev = () => config.domain
 
-const url = ( startDate='2017-06-01', endDate='2017-06-30', user=config.login ) => 
+const url = ( startDate, endDate, user=config.login ) => 
     isDev ? urlDev() : urlProd( startDate, endDate, user )
 
 const goal = ( user='someone.dsn.cir' ) => 
