a36381093bf877cdfaac044a42d0f0547ae72854
delete <pinheiro.llip@gmail.com>
2017-08-11 17:11:42 -0300

    add admin mode to top10

--- slackbot/commands/top10.js
+++ slackbot/commands/top10.js
@@ -1,15 +1,15 @@
 const messages = require('../messages')
 const emitter = require('../eventBus')
-const { getUser } = require('../auth')
 const { sumPontuation, minutesToPoints, sumTime } = require('../../src/filters')
 
 const get = require('../../src/request')
-const { auth, url, pointsMinute } = require('../../src/configs')
+const { auth, url, pointsMinute, admins } = require('../../src/configs')
 const parser = require('../../src/parser')
 const { goal, dsn, qld, startDate, endDate } = require('../../src/configs')
+const isAdmin = ( user ) => admins.includes( user ) 
 
 const requestIssue = (  username ) => {
-    const filterUrl = url( startDate, endDate, username )
+    const filterUrl = url( startDate(), endDate(), username )
     const headers = { 'Authorization': `Basic ${auth()}` }
     const options = { headers }
 
@@ -26,8 +26,9 @@ const jiraErrorMessage = ( response ) => {
 const reachedGoal = ( points, user ) => points > goal( user ) ? ':sunglasses:' :  ''
 const sortByPoints = ( people ) => [...people].sort( (a,b) => b.points - a.points );
 const printPerson = ( person, index ) => `${index}º - ${person.username}  ${reachedGoal( person.points, person.username )}`
+const printPersonAsAdmin = ( person, index ) => `${index + 1}º - ${person.username}  ${reachedGoal( person.points, person.username )} => ${person.points}`
 
-const mountResponse = ( people, channel ) => {
+const mountResponse = ( user, people, channel ) => {
     const peopleWithPoints = people.map( person =>  {
         return {
             username: person.username,
@@ -36,7 +37,7 @@ const mountResponse = ( people, channel ) => {
     })
     const peopleTop = sortByPoints( peopleWithPoints)
     const topTen = peopleTop
-        .map( printPerson )
+        .map( isAdmin(user) ? printPersonAsAdmin : printPerson )
         .slice(0, 10)
         .join('\n')
 
@@ -44,7 +45,7 @@ const mountResponse = ( people, channel ) => {
 }
 
 const top10 = ( message ) => {
-    const { channel } = message
+    const { channel, user } = message
     emitter.emit('SEND', messages('LOADING'), channel )
 
     const devs = dsn.map( dev => 
@@ -56,13 +57,13 @@ const top10 = ( message ) => {
     Promise.all(devs)
        .then(( devs ) => {
             emitter.emit('SEND', '*TOP 10 do desenvolvimento:* ', channel )
-            mountResponse( devs, channel )
+            mountResponse( user, devs, channel )
         })
 
     Promise.all(qlds)
        .then(( qlds ) => {
             emitter.emit('SEND', '*TOP 10 da qualidade:* ', channel )
-            mountResponse( qlds, channel )
+            mountResponse( user, qlds, channel )
         })
 }
 console.log('on TOP10')
